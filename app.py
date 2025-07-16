b34bf9fce8b846fb77e563da57935814bd183ad4
AntischA <antevidovicmail@gmail.com>
2025-07-16 15:11:11 +0200

    novo

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 def home():
     return render_template('index.html')
 
+@app.route('/cjenik')
+def cjenik():
+    return redirect("https://www.dotyourspot.com/Fnp253", code=302)  # 302 = Temporary Redirect
+
 
 @app.route("/<path:filename>")
 def serve_static(filename):
