0651b397666d0bdd0c60011770781ee641735302
AntischA <antevidovicmail@gmail.com>
2025-08-02 11:53:10 +0200

    grafdnbjhdd

--- app.py
+++ app.py
@@ -50,4 +50,8 @@ def serve_static(filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True  # 🔹 Omogućava automatski reload servera
+    )
